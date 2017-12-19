196eb5957cfd4a41e3b695ed816ea61190cc23c8
Long <longhui.yang@inveno.cn>
2017-12-19 10:48:35 +0800

    init project !

---
+++ java-logger/src/com/open/util/log/Logger.java
@@ -0,0 +1,497 @@
+package com.open.util.log;
+
+import com.open.util.log.base.ILog;
+import com.open.util.log.base.LogConfig;
+import com.open.util.log.impl.console.ConsLogger;
+import com.open.util.log.impl.file.FileLogger;
+import com.open.util.log.impl.net.TcpLogger;
+import com.open.util.log.impl.net.UdpLogger;
+import com.open.util.log.util.CfgParser;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+/**
+ * Created by long on 2017/9/13.
+ */
+
+public final class Logger{
+
+    private ArrayList<ILog> mLoggerList = new ArrayList<>();
+    private static Logger mLogger;
+    private static LogConfig mLogConfig;
+    private static boolean isInitialized = false;
+
+    public static LogConfig init(String config_file_path){
+        if(!isInitialized){
+            mLogConfig = LogConfig.parse(CfgParser.parseToMap(config_file_path));
+            if(null != mLogConfig){
+                if(mLogConfig.isEnable) {
+                    mLogger = new Logger();
+
+                    if((mLogConfig.common_mode & LogConfig.LOG_MODE_CONSOLE) == LogConfig.LOG_MODE_CONSOLE){
+                        mLogger.addLogger(new ConsLogger(mLogConfig.console_log_type));
+                    }
+
+                    if((mLogConfig.common_mode & LogConfig.LOG_MODE_FILE) == LogConfig.LOG_MODE_FILE){
+                        mLogger.addLogger(new FileLogger(mLogConfig.file_path,mLogConfig.file_name_formater,mLogConfig.file_size,mLogConfig.file_syn));
+                    }
+
+                    if((mLogConfig.common_mode & LogConfig.LOG_MODE_NET_TCP) == LogConfig.LOG_MODE_NET_TCP){
+                        mLogger.addLogger(new TcpLogger(mLogConfig.net_tcp));
+                    }
+
+                    if((mLogConfig.common_mode & LogConfig.LOG_MODE_NET_UDP) == LogConfig.LOG_MODE_NET_UDP){
+                        mLogger.addLogger(new UdpLogger(mLogConfig.net_udp));
+                    }
+
+                    mLogger.start();
+
+                }else{
+                    destroy();
+                }
+            } else{
+                destroy();
+            }
+            isInitialized = true;
+        }else{
+            //有可能有网络连接，再次初始化一次
+            if(null != mLogger){
+                mLogger.start();
+            }
+        }
+        return mLogConfig;
+    }
+
+    public static LogConfig updateConfig(String config_file_path){
+        destroy();
+        return init(config_file_path);
+    }
+
+    public static void destroy(){
+        if(null != mLogger){
+            mLogger.stop();
+        }
+        mLogConfig = null;
+        mLogger = null;
+        isInitialized = false;
+    }
+
+    //------------------------------------------
+    private void addLogger(ILog logger){
+        mLoggerList.add(logger);
+    }
+
+    private void start(){
+        for (ILog logger : mLogger.mLoggerList){
+            logger.start();
+        }
+    }
+
+    private void stop(){
+        for (ILog logger : mLogger.mLoggerList){
+            logger.stop();
+        }
+        mLogger = null;
+    }
+
+    //------------------------------------------
+    public static void v(String tag , String... kv) {
+        if(null == mLogger || kv.length == 0 || null == mLogConfig ||!mLogConfig.isPermitLevel(LogConfig.LOG_LEVEL_VERBOSE)){
+            return;
+        }
+
+        String trace = null;
+        if(mLogConfig.isCanFormatTag()){
+            String [] names = new String[6];
+            if(fillTraceNames(names)){
+                trace = mLogConfig.getTraceInfo(names);
+            }
+        }
+
+        String[] logKv = kv;
+        int size = mLogger.mLoggerList.size();
+        for (int i = 0; i < size; i++) {
+            mLogger.mLoggerList.get(i).v(LogConfig.LOG_LEVEL_VERBOSE,tag,trace,logKv);
+        }
+    }
+
+    public static void d(String tag  , String... kv) {
+        if(null == mLogger || kv.length == 0 || null == mLogConfig || !mLogConfig.isPermitLevel(LogConfig.LOG_LEVEL_DEBUG)){
+            return;
+        }
+
+        String trace = null;
+        if(mLogConfig.isCanFormatTag()){
+            String [] names = new String[6];
+            if(fillTraceNames(names)){
+                trace = mLogConfig.getTraceInfo(names);
+            }
+        }
+
+        String[] logKv = kv;
+        int size = mLogger.mLoggerList.size();
+        for (int i = 0; i < size; i++) {
+            mLogger.mLoggerList.get(i).d(LogConfig.LOG_LEVEL_DEBUG,tag,trace,logKv);
+        }
+    }
+
+    public static void i(String tag , String... kv) {
+        if(null == mLogger || kv.length == 0 || null == mLogConfig || !mLogConfig.isPermitLevel(LogConfig.LOG_LEVEL_INFO)){
+            return;
+        }
+
+        String trace = null;
+        if(mLogConfig.isCanFormatTag()){
+            String [] names = new String[6];
+            if(fillTraceNames(names)){
+                trace = mLogConfig.getTraceInfo(names);
+            }
+        }
+
+        String[] logKv = kv;
+        int size = mLogger.mLoggerList.size();
+        for (int i = 0; i < size; i++) {
+            mLogger.mLoggerList.get(i).i(LogConfig.LOG_LEVEL_INFO,tag,trace,logKv);
+        }
+    }
+
+    public static void w(String tag  , String... kv) {
+        if(null == mLogger || kv.length == 0 || null == mLogConfig || !mLogConfig.isPermitLevel(LogConfig.LOG_LEVEL_WARN)){
+            return;
+        }
+
+        String trace = null;
+        if(mLogConfig.isCanFormatTag()){
+            String [] names = new String[6];
+            if(fillTraceNames(names)){
+                trace = mLogConfig.getTraceInfo(names);
+            }
+        }
+
+        String[] logKv = kv;
+        int size = mLogger.mLoggerList.size();
+        for (int i = 0; i < size; i++) {
+            mLogger.mLoggerList.get(i).w(LogConfig.LOG_LEVEL_WARN,tag,trace,logKv);
+        }
+    }
+
+    public static  void e(String tag , String... kv) {
+        if(null == mLogger || kv.length == 0 || null == mLogConfig || !mLogConfig.isPermitLevel(LogConfig.LOG_LEVEL_ERROR)){
+            return;
+        }
+
+        String trace = null;
+        if(mLogConfig.isCanFormatTag()){
+            String [] names = new String[6];
+            if(fillTraceNames(names)){
+                trace = mLogConfig.getTraceInfo(names);
+            }
+        }
+
+        String[] logKv = kv;
+        int size = mLogger.mLoggerList.size();
+        for (int i = 0; i < size; i++) {
+            mLogger.mLoggerList.get(i).e(LogConfig.LOG_LEVEL_ERROR,tag,trace,logKv);
+        }
+    }
+
+    //------------------------------------------
+    public static void va(String author , String tag , String... kv) {
+        if(null == mLogger || kv.length == 0 || null == mLogConfig ||!mLogConfig.isPermit(author, LogConfig.LOG_LEVEL_VERBOSE)){
+            return;
+        }
+
+        String trace = null;
+        if(mLogConfig.isCanFormatTag()){
+            String [] names = new String[6];
+            if(fillTraceNames(names)){
+                trace = mLogConfig.getTraceInfo(names);
+            }
+        }
+
+        String[] logKv = kv;
+        int size = mLogger.mLoggerList.size();
+        for (int i = 0; i < size; i++) {
+            mLogger.mLoggerList.get(i).v(LogConfig.LOG_LEVEL_VERBOSE,tag,trace,logKv);
+        }
+    }
+
+    public static void da(String author , String tag  , String... kv) {
+        if(null == mLogger || kv.length == 0 || null == mLogConfig || !mLogConfig.isPermit(author, LogConfig.LOG_LEVEL_DEBUG)){
+            return;
+        }
+
+        String trace = null;
+        if(mLogConfig.isCanFormatTag()){
+            String [] names = new String[6];
+            if(fillTraceNames(names)){
+                trace = mLogConfig.getTraceInfo(names);
+            }
+        }
+
+        String[] logKv = kv;
+        int size = mLogger.mLoggerList.size();
+        for (int i = 0; i < size; i++) {
+            mLogger.mLoggerList.get(i).d(LogConfig.LOG_LEVEL_DEBUG,tag,trace,logKv);
+        }
+    }
+
+    public static void ia(String author  , String tag , String... kv) {
+        if(null == mLogger || kv.length == 0 || null == mLogConfig || !mLogConfig.isPermit(author, LogConfig.LOG_LEVEL_INFO)){
+            return;
+        }
+
+        String trace = null;
+        if(mLogConfig.isCanFormatTag()){
+            String [] names = new String[6];
+            if(fillTraceNames(names)){
+                trace = mLogConfig.getTraceInfo(names);
+            }
+        }
+
+        String[] logKv = kv;
+        int size = mLogger.mLoggerList.size();
+        for (int i = 0; i < size; i++) {
+            mLogger.mLoggerList.get(i).i(LogConfig.LOG_LEVEL_INFO,tag,trace,logKv);
+        }
+    }
+
+    public static void wa(String author , String tag  , String... kv) {
+        if(null == mLogger || kv.length == 0 || null == mLogConfig || !mLogConfig.isPermit(author, LogConfig.LOG_LEVEL_WARN)){
+            return;
+        }
+
+        String trace = null;
+        if(mLogConfig.isCanFormatTag()){
+            String [] names = new String[6];
+            if(fillTraceNames(names)){
+                trace = mLogConfig.getTraceInfo(names);
+            }
+        }
+
+        String[] logKv = kv;
+        int size = mLogger.mLoggerList.size();
+        for (int i = 0; i < size; i++) {
+            mLogger.mLoggerList.get(i).w(LogConfig.LOG_LEVEL_WARN,tag,trace,logKv);
+        }
+    }
+
+    public static  void ea(String author  , String tag , String... kv) {
+        if(null == mLogger || kv.length == 0 || null == mLogConfig || !mLogConfig.isPermit(author, LogConfig.LOG_LEVEL_ERROR)){
+            return;
+        }
+
+        String trace = null;
+        if(mLogConfig.isCanFormatTag()){
+            String [] names = new String[6];
+            if(fillTraceNames(names)){
+                trace = mLogConfig.getTraceInfo(names);
+            }
+        }
+
+        String[] logKv = kv;
+        int size = mLogger.mLoggerList.size();
+        for (int i = 0; i < size; i++) {
+            mLogger.mLoggerList.get(i).e(LogConfig.LOG_LEVEL_ERROR,tag,trace,logKv);
+        }
+    }
+    
+    //------------------------------------------
+    public static void v(String author , String tag , Object... kv) {
+        if(null == mLogger || kv.length == 0 || null == mLogConfig ||!mLogConfig.isPermit(author, LogConfig.LOG_LEVEL_VERBOSE)){
+            return;
+        }
+
+        String trace = null;
+        if(mLogConfig.isCanFormatTag()){
+            String [] names = new String[6];
+            if(fillTraceNames(names)){
+                trace = mLogConfig.getTraceInfo(names);
+            }
+        }
+
+        String[] logKv = transformMessage(kv);
+        int size = mLogger.mLoggerList.size();
+        for (int i = 0; i < size; i++) {
+            mLogger.mLoggerList.get(i).v(LogConfig.LOG_LEVEL_VERBOSE,tag,trace,logKv);
+        }
+    }
+
+    public static void d(String author , String tag  , Object... kv) {
+        if(null == mLogger || kv.length == 0 || null == mLogConfig || !mLogConfig.isPermit(author, LogConfig.LOG_LEVEL_DEBUG)){
+            return;
+        }
+
+        String trace = null;
+        if(mLogConfig.isCanFormatTag()){
+            String [] names = new String[6];
+            if(fillTraceNames(names)){
+                trace = mLogConfig.getTraceInfo(names);
+            }
+        }
+
+        String[] logKv = transformMessage(kv);
+        int size = mLogger.mLoggerList.size();
+        for (int i = 0; i < size; i++) {
+            mLogger.mLoggerList.get(i).d(LogConfig.LOG_LEVEL_DEBUG,tag,trace,logKv);
+        }
+    }
+
+    public static void i(String author  , String tag , Object... kv) {
+        if(null == mLogger || kv.length == 0 || null == mLogConfig || !mLogConfig.isPermit(author, LogConfig.LOG_LEVEL_INFO)){
+            return;
+        }
+
+        String trace = null;
+        if(mLogConfig.isCanFormatTag()){
+            String [] names = new String[6];
+            if(fillTraceNames(names)){
+                trace = mLogConfig.getTraceInfo(names);
+            }
+        }
+
+        String[] logKv = transformMessage(kv);
+        int size = mLogger.mLoggerList.size();
+        for (int i = 0; i < size; i++) {
+            mLogger.mLoggerList.get(i).i(LogConfig.LOG_LEVEL_INFO,tag,trace,logKv);
+        }
+    }
+
+    public static void w(String author , String tag  , Object... kv) {
+        if(null == mLogger || kv.length == 0 || null == mLogConfig || !mLogConfig.isPermit(author, LogConfig.LOG_LEVEL_WARN)){
+            return;
+        }
+
+        String trace = null;
+        if(mLogConfig.isCanFormatTag()){
+            String [] names = new String[6];
+            if(fillTraceNames(names)){
+                trace = mLogConfig.getTraceInfo(names);
+            }
+        }
+
+        String[] logKv = transformMessage(kv);
+        int size = mLogger.mLoggerList.size();
+        for (int i = 0; i < size; i++) {
+            mLogger.mLoggerList.get(i).w(LogConfig.LOG_LEVEL_WARN,tag,trace,logKv);
+        }
+    }
+
+    public static  void e(String author  , String tag , Object... kv) {
+        if(null == mLogger || kv.length == 0 || null == mLogConfig || !mLogConfig.isPermit(author, LogConfig.LOG_LEVEL_ERROR)){
+            return;
+        }
+
+        String trace = null;
+        if(mLogConfig.isCanFormatTag()){
+            String [] names = new String[6];
+            if(fillTraceNames(names)){
+                trace = mLogConfig.getTraceInfo(names);
+            }
+        }
+
+        String[] logKv = transformMessage(kv);
+        int size = mLogger.mLoggerList.size();
+        for (int i = 0; i < size; i++) {
+            mLogger.mLoggerList.get(i).e(LogConfig.LOG_LEVEL_ERROR,tag,trace,logKv);
+        }
+    }
+
+    //------------------------------------------
+    private static boolean fillTraceNames(String[] names){
+        StackTraceElement[] sts = Thread.currentThread().getStackTrace();
+        if (sts != null) {
+            for (StackTraceElement st:sts) {
+                if (st.isNativeMethod()) {
+                    continue;
+                }
+
+                if (st.getClassName().equals(Thread.class.getName())) {
+                    continue;
+                }
+
+                if (st.getClassName().equals(Logger.class.getName())) {
+                    continue;
+                }
+
+                names[ILog.INDEX_FILENAME]   = st.getFileName();
+                names[ILog.INDEX_CLASSNAME]  = st.getClassName();
+                names[ILog.INDEX_METHODNAME] = st.getMethodName()+"()";
+                names[ILog.INDEX_LINENUMBER] = ""+st.getLineNumber();
+                names[ILog.INDEX_PID]        = ""+getPid();
+                names[ILog.INDEX_TID]        = ""+Thread.currentThread().getId();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static int getPid() {
+        RuntimeMXBean runtime = ManagementFactory.getRuntimeMXBean();
+        String name = runtime.getName(); // format: "pid@hostname"
+        try {
+            return Integer.parseInt(name.substring(0, name.indexOf('@')));
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+    //-------------------------------------------------------------------
+    private static final int JSON_INDENT = 2;
+    private static String[] transformMessage(Object[] input){
+        String[] ret = new String[input.length];
+        for (int i = 0 ;i < input.length; i++){
+            ret[i] = transformMessage(input[i]);
+        }
+        return ret;
+    }
+
+    private static String transformMessage(Object input){
+        try{
+            if(input instanceof String){
+                return (String) input;
+            }else if(input instanceof JSONObject){
+                JSONObject jsonObject = (JSONObject) input;
+                return jsonObject.toString(JSON_INDENT);
+            }else if(input instanceof JSONArray){
+                JSONArray jsonArray = (JSONArray) input;
+                return jsonArray.toString(JSON_INDENT);
+            }else if (input instanceof Throwable) {
+                return getStackTraceString((Throwable) input);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null != input ? input.toString() : "Null";
+    }
+    
+    static String getStackTraceString(Throwable tr) {
+        if (tr == null) {
+          return "";
+        }
+
+        // This is to reduce the amount of log spew that apps do in the non-error
+        // condition of the network being unavailable.
+        Throwable t = tr;
+        while (t != null) {
+          if (t instanceof UnknownHostException) {
+            return "";
+          }
+          t = t.getCause();
+        }
+
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        tr.printStackTrace(pw);
+        pw.flush();
+        return sw.toString();
+      }
+}
---
+++ java-logger/src/com/open/util/log/impl/net/TcpLogger.java
@@ -0,0 +1,188 @@
+package com.open.util.log.impl.net;
+
+import com.open.net.client.impl.tcp.nio.TcpNioClient;
+import com.open.net.client.structures.BaseClient;
+import com.open.net.client.structures.BaseMessageProcessor;
+import com.open.net.client.structures.TcpAddress;
+import com.open.net.client.structures.message.Message;
+import com.open.util.log.base.ILog;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+
+/**
+ * 网络日志
+ * Created by long on 2017/9/13.
+ */
+
+public final class TcpLogger implements ILog {
+
+    @Override
+    public void v(int priority, String tag, String trace, String... kv) {
+        println(LOG_VERBOSE,tag,trace,kv);
+    }
+
+    @Override
+    public void d(int priority, String tag, String trace, String... kv) {
+        println(LOG_DEBUG,tag,trace,kv);
+    }
+
+    @Override
+    public void i(int priority, String tag, String trace, String... kv) {
+        println(LOG_INFO,tag,trace,kv);
+    }
+
+    @Override
+    public void w(int priority, String tag, String trace, String... kv) {
+        println(LOG_WARN,tag,trace,kv);
+    }
+
+    @Override
+    public void e(int priority, String tag, String trace, String... kv) {
+        println(LOG_ERROR,tag,trace,kv);
+    }
+
+    @Override
+    public void start() {
+        mNioClient.connect();
+    }
+
+    @Override
+    public void stop() {
+        mNioClient.disconnect();
+    }
+
+    //------------------------------------------------------------
+    private StringBuilder builder = new StringBuilder(LOGGER_ENTRY_MAX_LEN_FIX/2);
+    private TcpNioClient mNioClient ;
+
+
+    private BaseMessageProcessor mMessageProcessor =new BaseMessageProcessor() {
+
+        @Override
+        public void onReceiveMessages(BaseClient mClient, final LinkedList<Message> mQueen) {
+            //对收到的信息不处理
+        }
+    };
+
+    public TcpLogger(TcpAddress[] tcpArray) {
+        mNioClient = new TcpNioClient(mMessageProcessor,null);
+        mNioClient.setConnectAddress(tcpArray);
+    }
+
+    //------------------------------------------------------------
+    private boolean println(String priority, String tag, String trace, String... kv){
+        boolean ret = false;
+        builder.setLength(0);
+        if(kv.length>1){
+            int count = 0;
+            for (int i = 0; i < kv.length; i++) {
+                if(null == kv[i]){
+                    continue;
+                }
+
+                int length = kv[i].length();
+                //一旦加上这个msg 大于 LOGGER_ENTRY_MAX_LEN_FIX ，1.需要马上打印之前的日志，打印后清空; 2.接着重新填入
+                if((count + length) > LOGGER_ENTRY_MAX_LEN_FIX){
+
+                    //1. 把上次记录先打印
+                    if(builder.length()>0){
+                        ret = log(priority,tag,trace, builder.toString());
+                        if(!ret){
+                            return ret;
+                        }
+                        builder.delete(0, builder.length());
+                        count = 0;
+                    }
+
+                    //2. 判断本次要打印的记录是否也是大于 LOGGER_ENTRY_MAX_LEN_FIX
+                    if(length>LOGGER_ENTRY_MAX_LEN_FIX){
+                        int page = length % LOGGER_ENTRY_MAX_LEN_FIX == 0 ? length/LOGGER_ENTRY_MAX_LEN_FIX : (length/LOGGER_ENTRY_MAX_LEN_FIX + 1);
+                        for(int j = 1;j< page;j++){
+                            ret = log(priority,tag,trace,kv[i].substring((j-1)*LOGGER_ENTRY_MAX_LEN_FIX,j*LOGGER_ENTRY_MAX_LEN_FIX));
+                            if(!ret){
+                                return ret;
+                            }
+                        }
+
+                        count += length;
+                        builder.append(kv[i].substring((page-1)*LOGGER_ENTRY_MAX_LEN_FIX,length));
+
+                    }else{
+                        count += length;
+                        builder.append(kv[i]);
+                    }
+
+                }else{
+
+                    count += kv[i].length();
+                    builder.append(kv[i]);
+                }
+            }
+
+            if(builder.length()>0){
+                ret = log(priority,tag,trace, builder.toString());
+                if(!ret){
+                    return ret;
+                }
+            }
+        }else{
+            int length = kv[0].length();
+            if(length>LOGGER_ENTRY_MAX_LEN_FIX){
+                int page = length % LOGGER_ENTRY_MAX_LEN_FIX == 0 ? length/LOGGER_ENTRY_MAX_LEN_FIX : (length/LOGGER_ENTRY_MAX_LEN_FIX + 1);
+                for(int j = 1;j< page;j++){
+                    ret = log(priority,tag,trace,kv[0].substring((j-1)*LOGGER_ENTRY_MAX_LEN_FIX,j*LOGGER_ENTRY_MAX_LEN_FIX));
+                    if(!ret){
+                        return ret;
+                    }
+                }
+
+                ret = log(priority,tag,trace,kv[0].substring((page-1)*LOGGER_ENTRY_MAX_LEN_FIX,length));
+                if(!ret){
+                    return ret;
+                }
+            }else{
+                ret = log(priority,tag,trace,kv[0]);
+                if(!ret){
+                    return ret;
+                }
+            }
+        }
+        return ret;
+    }
+
+    private boolean log(String priority, String tag,String trace, String kv){
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+        String time = sdf.format(new Date());
+
+        StringBuilder sb = new StringBuilder(time.length()
+                + (null != priority ? priority.length() + 1 : 0)
+                + (null != tag ? tag.length()+ 1 : 0 )
+                + (null != trace ? trace.length()+ 1 : 0)
+                + (null != kv ? kv.length()+ 1 :0));
+        sb.append(time);
+        if(null != priority && priority.length()>0){
+            sb.append(" ");
+            sb.append(priority);
+        }
+        if(null != tag && tag.length()>0){
+            sb.append(" ");
+            sb.append(tag);
+        }
+        if(null != trace && trace.length()>0){
+            sb.append(" ");
+            sb.append(trace);
+        }
+        if(null != kv && kv.length()>0){
+            sb.append(" ");
+            sb.append(kv);
+        }
+        sb.append(NEW_LINE);
+
+        mMessageProcessor.send(mNioClient,sb.toString().getBytes());
+        return true;
+    }
+
+}
